81b95f92f7de13e22184c70d6ece2e7ad28a2a37
Hasibul POlok <hasibulpolok.bdn@gmail.com>
2022-06-15 03:41:50 +0600

    services section complete

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,10 +1,8 @@
 import { Grid } from '@mui/material';
 import React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography'
 
 
@@ -12,7 +10,7 @@ const Service = (props) => {
     const { name, description, img } = props.service;
     return (
         <Grid item xs={2} sm={4} md={4}>
-            <Card sx={{ minWidth: 275 }}>
+            <Card sx={{ minWidth: 275, border:0, boxShadow:0 }}>
                 <CardMedia
                     component="img"
                     style={{ width: 'auto', height:'80px', margin:'0 auto' }}
@@ -25,13 +23,11 @@ const Service = (props) => {
                         {name}
                     </Typography>
 
-                    <Typography variant="body2">
+                    <Typography variant="body2" color="text.secondary">
                         {description}
                     </Typography>
                 </CardContent>
-                <CardActions>
-                    <Button size="small">Learn More</Button>
-                </CardActions>
+                
             </Card>
         </Grid>
     );
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { experimentalStyled as styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { Container } from '@mui/material';
@@ -36,6 +36,12 @@ const Services = () => {
 
         <Box sx={{ flexGrow: 1 }}>
             <Container>
+                <Typography sx={{fontWeight : 500 ,color:"success.main", m : 2}} variant="h6" component="div">
+                    OUR SERVICES
+                </Typography>
+                <Typography sx={{fontWeight : 600 , m : 3}} variant="h4" component="div">
+                   Services We Provide
+                </Typography>
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                     {
                         services.map(service => <Service
